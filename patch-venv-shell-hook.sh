cb68f80ec17513174601fcd4ad5c8586dc54fc56
László Vaskó <1771332+vlaci@users.noreply.github.com>
2025-02-26 17:18:32 +0100

    Properly iterate on patches

--- patch-venv-shell-hook.sh
+++ patch-venv-shell-hook.sh
@@ -13,7 +13,10 @@ fi
 export UV_LINK_MODE=copy
 
 patchVenvShellHook() {
-    for p in "${venvPatches[@]-}"; do
+    local venvPatchesArray=()
+    concatTo venvPatchesArray venvPatches
+
+    for p in "${venvPatchesArray[@]}"; do
         if ! 1>/dev/null @patch@/bin/patch -f -R -p1 -s --dry-run -d "$venvDir/@pythonSitePackages@" -i "$p"; then
             echo "applying $p"
             @patch@/bin/patch -f -p1 -d "$venvDir/@pythonSitePackages@" -i "$p"
